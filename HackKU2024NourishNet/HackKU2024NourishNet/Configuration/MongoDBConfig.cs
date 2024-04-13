e1229888e60ea463d875b10e1549d6bfc1bcc2cf
Noah <noahmosel73@gmail.com>
2024-04-13 01:12:15 -0500

    MongoDBConfig

---
+++ HackKU2024NourishNet/HackKU2024NourishNet/Configuration/MongoDBConfig.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+
+namespace HackKU2024NourishNet.Configuration
+{
+    public class MongoDBConfig
+    {
+        static MongoDBConfig mongoDBConfig;
+        public static MongoDBConfig GetMongoDBConfig 
+        { 
+            get
+            {
+                if (mongoDBConfig == null)
+                {
+                    mongoDBConfig = new MongoDBConfig();
+                }
+                return mongoDBConfig;
+            }
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("MongoDBConnectionString").Replace("<password>", Environment.GetEnvironmentVariable("MongoDBPassword"));
+            }
+        }
+    }
+}
